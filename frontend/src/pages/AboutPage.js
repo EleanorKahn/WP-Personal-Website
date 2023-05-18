b59889bae434003ef1ece659edb3a2cc78fe0ced
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-18 12:30:44 -0700

    site skeleton, style selector skeleton

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const AboutPage = () => {
     return (
-        <div>
+        <div className='about-page'>
             <h2>I'm Eleanor</h2>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
