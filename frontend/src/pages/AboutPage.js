2f7b85dea266ed6848b99e9dec92f1c033900372
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-24 00:11:58 -0700

    margin at bottom of footer fixed

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
 
 const AboutPage = () => {
     return (
         <div className='about-page'>
-            <Row className='grid row'>
-                <Col className='col-md-6'>
-                    <h2>Hi, I'm Eleanor</h2>
-                </Col>
-                <Col className='col-md-6'>
-                    <article className='summary'>
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    </article>
-                </Col>
-            </Row>
+            <section className='grid'>
+                <h2>Hi, I'm Eleanor</h2>
+                <article className='summary'>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                </article>
+            </section>
         </div>
     );
 };
