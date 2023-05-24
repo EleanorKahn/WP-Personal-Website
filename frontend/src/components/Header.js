448d9ea4d7045bbaa35b69052ca0154eb54cbee7
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-23 22:05:06 -0700

    fixed white screen of death, header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
-import { Navbar, Nav, NavbarBrand, Collapse, NavbarToggler } from "reactstrap";
+import { Navbar, Nav, NavbarBrand, Collapse, NavbarToggler, Container, Row, Col } from "reactstrap";
 
 const Header = () => {
     const [menuOpen, setmenuOpen] = useState(true);
@@ -8,22 +8,36 @@ const Header = () => {
 
     return (
         <div className="header">
-            <Navbar className="nav" id="nav" expand="sm" style={{}}>
-                <NavbarBrand href="/">
-                    <h1 className='header-icon'>
-                        <NavLink className='nav-link' to="/">&lt;<span className=''>EK</span>/&gt;</NavLink>
-                    </h1>
-                </NavbarBrand>
-                <NavbarToggler onClick={() => toggleNavbar()}/>
-                <Collapse isOpen={!menuOpen} navbar>
-                    <Nav className="horizontal navigation-menu" navbar>
-                        <NavLink className='nav-link' to="/">Home</NavLink>
-                        <NavLink className='nav-link' to="/about">About Me</NavLink>
-                        <NavLink className="nav-link" to="/">My Work</NavLink>
-                        <NavLink className="nav-link" to="/">Get in Touch</NavLink>
-                    </Nav>
-                </Collapse>
-            </Navbar>
+            <Container>
+                <Row>
+                <Navbar className="nav " id="nav" expand="md" style={{}}>
+                    <Col className='md-6' md="6">
+                    <NavbarBrand href="/">
+                        <h1 className='header-icon'>
+                            <NavLink className='nav-link' to="/">&lt;<span className=''>EK</span>/&gt;</NavLink>
+                        </h1>
+                    </NavbarBrand>
+                    </Col>
+                    <NavbarToggler onClick={() => toggleNavbar()}/>
+                    <Collapse isOpen={!menuOpen} navbar>
+                        <Nav className="horizontal navigation-menu" navbar>
+                            <Col className='md-2'>
+                            <NavLink className='nav-link' to="/">Home</NavLink>
+                            </Col>
+                            <Col className='md-2' md="2">
+                            <NavLink className='nav-link' to="/about">About Me</NavLink>
+                            </Col>
+                            <Col className='md-2'>
+                            <NavLink className="nav-link" to="/">My Work</NavLink>
+                            </Col>
+                            <Col className='md-2'>
+                            <NavLink className="nav-link" to="/">Get in Touch</NavLink>
+                            </Col>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+                </Row>
+            </Container>
         </div>
     );
 };
