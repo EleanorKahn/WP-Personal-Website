459351e85b07a76854528912570496ce58c71a19
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-24 19:54:09 -0700

    footer style troubleshooting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import "./css/style.css";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
