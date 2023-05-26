24fa9ca857833fa4f8ab0455a0f4af4759607210
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-25 18:08:08 -0700

    data from wp api correctly displayed in console.

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AboutPage = () => {
+    const [aboutTitle, setAboutTitle] = useState('');
+    const [aboutContent, setAboutContent] = useState('');
+
+    useEffect(() => {
+        requestAbout();
+    }, []);
+
+    const requestAbout = async() => {
+        try {
+            const response = await fetch("http://localhost:8000/wp-json/wp/v2/aboutme");
+            const data = await response.json();
+            const thisData = data[0];
+            const title = thisData.title;
+            const content = thisData.content;
+            setAboutTitle(data.title);
+            setAboutContent(data.content);
+            console.log(data);
+            console.log(thisData);
+            return data;
+        } catch(error) {
+            console.log(`there has been an error! ${error}`);
+        }
+    }
+
     return (
         <div className='about-page'>
             <section className='grid'>
-                <h2>Hi, I'm Eleanor</h2>
+                <h2>{aboutTitle}</h2>
                 <article className='summary'>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <p>{aboutContent}</p>
                 </article>
             </section>
         </div>
