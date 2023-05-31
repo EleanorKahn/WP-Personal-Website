f1be468c90a35114ef91137d7df1e1ea7fc04a02
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-30 23:55:33 -0700

    setting up custom menu

--- wp-content/themes/sunsetTheme/header.php
+++ wp-content/themes/sunsetTheme/header.php
@@ -30,7 +30,18 @@
 			</button>
 
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
-				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
+				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >	
+                
+                <?php
+                    wp_nav_menu(
+                        array(
+                            'menu' => 'primary',
+                            'container' => '',
+                            'theme_location' => 'primary',
+                            'items_wrap' => '<ul id="" class="navbar-nav flex-column text-sm-center text-md-left">&3$s</ul>'
+                        )
+                    );
+                ?>
 				
 				<ul class="navbar-nav flex-column text-sm-center text-md-left">
 					<li class="nav-item active">
