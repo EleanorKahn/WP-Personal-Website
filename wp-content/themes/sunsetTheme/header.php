b81ffab243090abe661897c5ba1c0959602f9b20
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-31 19:42:06 -0700

    more dynamic menu

--- wp-content/themes/sunsetTheme/header.php
+++ wp-content/themes/sunsetTheme/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
-    <link rel="shortcut icon" href="images/logo.png"> 
+    <link rel="shortcut icon" href="/wp-content/themes/sunsetTheme/assets/images/logo.png"> 
     
     <!-- FontAwesome CSS-->
 	<!-- Bootstrap CSS--> 
