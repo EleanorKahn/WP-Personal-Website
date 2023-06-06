5d3f3a84fb67630f3a64898902922281d2b1fbab
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-06-06 16:55:29 -0700

    archives

--- wp-content/themes/sunsetTheme/index.php
+++ wp-content/themes/sunsetTheme/index.php
@@ -1,8 +1,22 @@
 <?php
     get_header();
 ?>
+    
+		<article class="content px-3 py-5 p-md-5">
+            
+            <?php
+                if( have_posts() ){
+                    while( have_posts() ){
 
-<div id='landing-page'></div>
+                        the_post();
+                        get_template_part( 'template-parts/content', 'archive' );
+                    }
+                }
+            ?>
+
+
+	    </article>
+	    
 <?php
     get_footer();
 ?>
