9a6c8ade8e15fe3b3d3709afb215e76f4661932c
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-06-07 13:53:43 -0700

    build output

--- wp-content/themes/myResume/assets/build/index.asset.php
+++ wp-content/themes/myResume/assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '8a1cf72cd0e163b1c75a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7727c7d8f76b542a4c3f');
