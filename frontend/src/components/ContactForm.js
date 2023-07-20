1eedc0ecbd6e33e287e97b42be7b84a5c4fccd47
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-07-19 22:49:00 -0700

    form grid

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Input, Label, Form, FormGroup } from 'reactstrap';
+import { Input, Label, Form, FormGroup, Button, Row, Col, Container } from 'reactstrap';
 
 // inital mockup
 const ContactForm = () => {
@@ -8,33 +8,61 @@ const ContactForm = () => {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(firstName);
+        console.log(lastName);
+        console.log(email);
+        console.log(message);
+
+    }
+
     return (
         <div className='contact-form'>
+            <Container>
             <Form className='form'>
+                <Row>
+                <Col md={6}>
+                    <FormGroup>
+                        
+                            <Label                      
+                                htmlFor='firstName'
+                                className='form-label'
+                            >
+                                First name 
+                            </Label>
+                            <Input 
+                                type='text'
+                                id='firstName'
+                                value={firstName}
+                                onChange={(firstName) => setFirstName(firstName)}
+                            />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                    <FormGroup>
+                        
+                            <Label                      
+                                htmlFor='lastName'
+                                className='form-label'
+                            >
+                                Last name 
+                            </Label>
+                            <Input 
+                                type='text'
+                                id='lastName'
+                                value={lastName}
+                                onChange={(value) => setLastName(value)}
+                            />
+                            </FormGroup>
+                        </Col>
+                    
+                </Row>
                 <FormGroup>
-                    <Label htmlFor='firstName'>
-                        First name 
-                    </Label>
-                    <Input 
-                        type='text'
-                        id='firstName'
-                        value={firstName}
-                        onChange={(value) => setFirstName(value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor='lastName'>
-                        Last name 
-                    </Label>
-                    <Input 
-                        type='text'
-                        id='lastName'
-                        value={lastName}
-                        onChange={(value) => setLastName(value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor='email'>
+                    <Label 
+                        htmlFor='email'
+                        className='form-label'
+                    >
                         Email
                     </Label>
                     <Input 
@@ -45,18 +73,27 @@ const ContactForm = () => {
                     />
                 </FormGroup>
                 <FormGroup>
-                    <Label htmlFor='message'>
+                    <Label 
+                        htmlFor='message'
+                        className='form-label'
+                        >
                         Your message
                     </Label>
                     <Input 
                         type='textarea'
                         id='message'
                         value={message}
-                        onChange={(value) => setEmail(value)}
+                        onChange={(value) => setMessage(value)}
                     />
                 </FormGroup>
-                <button type='submit'>Submit</button>
+                <Button 
+                    type='submit'
+                    onClick={(e) => handleSubmit(e)}
+                >
+                    Submit
+                </Button>
             </Form>
+            </Container>
         </div>
     );
 };
