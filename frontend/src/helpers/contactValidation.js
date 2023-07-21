8fcac758b6b68993ae9abf5caf95acb122e53296
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-07-20 23:57:17 -0700

    form validation roadmap

---
+++ frontend/src/helpers/contactValidation.js
@@ -0,0 +1,5 @@
+// overview 
+// first name and last name must be 2 letters or greater, and contain only letters, hyphons, commas
+// email must contain @, and have a legit domain name
+// goal - to have a certain character limit for the message, and have the UI display number of remaining characters
+// all 4 fields are required 
