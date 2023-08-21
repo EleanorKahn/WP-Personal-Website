782a0360074e69e55a236ff7b7c041009eaf04d5
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-08-21 13:01:20 -0700

    validation refactoring

--- frontend/src/pages/MyWorkPage.js
+++ frontend/src/pages/MyWorkPage.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NotFound from '../components/NotFound';
 
 const MyWorkPage = () => {
+
+    
     return (
         <div>
             <NotFound />
