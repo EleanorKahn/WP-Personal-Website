c2a642c61814528549a16835f7bed1ec8cc0d3bc
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-08-23 15:45:50 -0700

    projects post test:

--- frontend/src/pages/MyWorkPage.js
+++ frontend/src/pages/MyWorkPage.js
@@ -3,7 +3,21 @@ import NotFound from '../components/NotFound';
 
 const MyWorkPage = () => {
     const [image, setImage] = useState(null);
-    const [title, setTitle] = useState("");
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    let projects = null;
+
+    const requestWork = async () => {
+        const URL = 'http://localhost:8000/wp-json/wp/v2/projects/';
+        const response = await fetch(URL);
+        const data = await response.json();
+        projects = data;
+        console.log(projects);
+    }
+
+    useEffect(() => {
+        requestWork();
+    },[])
 
     return (
         <div className='my-work-page'>
